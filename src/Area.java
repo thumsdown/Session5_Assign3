ae669263f59405ee188e07201fa108570d163e09
Vinit <thumsdown@gmail.com>
2016-02-07 20:13:50 +0530

    5th Session 3rd Assignment

---
+++ src/Area.java
@@ -0,0 +1,11 @@
+
+public class Area {
+	public static void main(String[] args) {
+		OperatorOverloading op = new OperatorOverloading();
+		
+		System.out.println("Area of Square = "+ op.calculateArea(5));
+		
+		System.out.println("Area of Rectangle = "+ op.calculateArea(4, 5));
+	}
+		
+}
---
+++ src/OperatorOverloading.java
@@ -0,0 +1,11 @@
+
+public class OperatorOverloading {
+	public int calculateArea( int a )
+	{
+		return a*a;
+	}
+	public int calculateArea(int a, int b)
+	{
+		return a*b;
+	}
+}
